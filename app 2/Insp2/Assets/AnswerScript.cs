73bc8608e3f2f8d15b7bfa05f8f747fed5d3b7cb
Guru Prakash G <guruthestudent@gmail.com>
2021-05-10 18:08:49 +0530

    Quizv1
    Exercise1 Completed.

---
+++ app 2/Insp2/Assets/AnswerScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnswerScript : MonoBehaviour
+{
+    public bool isCorrect = false;
+    public QuizManager quizManager;
+
+
+    public void Answer()
+    {
+        if(isCorrect)
+        {
+        
+            Debug.Log("Correct Answer");
+            quizManager.correct();
+        }
+        else
+        {
+            Debug.Log("Wrong Answer");
+            quizManager.wrong();
+        }
+    }
+
+}
---
+++ app 2/Insp2/Assets/QuestionAndAnswers.cs
@@ -0,0 +1,8 @@
+﻿
+[System.Serializable]
+public class QuestionAndAnswers
+{
+    public string Question;
+    public string[] Answers;
+    public int CorrectAnswer;
+}
