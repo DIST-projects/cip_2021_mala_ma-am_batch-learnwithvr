6c398ce2879ca9685c130ed55b1e34d17c077064
Guru Prakash G <guruthestudent@gmail.com>
2021-05-12 16:54:41 +0530

    Created Menu

---
+++ app 2/Insp2/Assets/SceneController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public void Exercise1()
+    {
+         SceneManager.LoadScene("SampleScene");
+    }
+    public void Exercise2()
+    {
+         SceneManager.LoadScene("SampleScene");
+    }
+    public void Exercise3()
+    {
+         SceneManager.LoadScene("SampleScene");
+    }
+    public void Exercise4()
+    {
+         SceneManager.LoadScene("SampleScene");
+    }
+}
